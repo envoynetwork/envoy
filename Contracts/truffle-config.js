ba8971b21a31f3c42aec06484a4263032c943132
Niel Deckx <>
2021-10-23 12:38:45 +0200

    Truffle config for mainnet

--- Contracts/truffle-config.js
+++ Contracts/truffle-config.js
@@ -37,7 +37,7 @@ module.exports = {
       network_id: 4
     },
 
-    // BSC test
+    // BSC Testnet
     bscTestnet: {
       provider: () => new HDWalletProvider(mnemonic, `https://data-seed-prebsc-1-s1.binance.org:8545`),
       network_id: 97,
@@ -46,8 +46,14 @@ module.exports = {
       skipDryRun: true
     },
 
-    // Mainnet
-    // TODO
+    // BSC Mainnet
+    bscMainnet: {
+      provider: () => new HDWalletProvider(mnemonic, `https://bsc-dataseed1.binance.org`),
+      network_id: 56,
+      confirmations: 10,
+      timeoutBlocks: 200,
+      skipDryRun: true
+    },
 
   },
 
